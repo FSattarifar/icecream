45392c3cee6c84b54689ea85272d9db7f524c4a2
Nazanin545 <104799080+Nazanin545@users.noreply.github.com>
2025-05-16 02:23:42 +0430

    edit html

--- romyk/header.php
+++ romyk/header.php
@@ -37,7 +37,7 @@ session_start();
     <div class="header_section header_bg">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="index.html"><img src="images/logo.png"></a>
+                <a class="navbar-brand" href="index.php"><img src="images/logo.png"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
